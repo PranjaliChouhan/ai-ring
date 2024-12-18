963d93f29135c80955b1893b6f1a1a594ebec06d
PranjaliChouhan <pranjalichouhan4@gmail.com>
2024-12-18 22:42:02 +0530

    feat:icon update

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,14 +1,8 @@
 import bybit from '@/assets/icons/bybit.svg'
 
-import bitget from '@/assets/icons/bitget.svg'
-// import bitmart from '@/assets/icons/bitmart.svg'
 import pancakeswap from '@/assets/icons/pancakeswap.svg'
 import mexc from '@/assets/icons/mexc.svg'
-import uniswap from '@/assets/icons/uniswap.svg'
-import bitrue from '@/assets/icons/bitrue.svg'
-import raydium from '@/assets/icons/raydium.svg'
 import Gate from '@/assets/icons/logo.svg'
-
 const partners = [
   {
     name: 'Bybit',
@@ -19,14 +13,7 @@ const partners = [
     icon: 'https://res.cloudinary.com/dfzcr2ch4/image/upload/v1717705783/Screenshot_2024-06-04_at_10.31_1_ntlhip.webp',
   },
 
-  {
-    name: 'Bitget',
-    icon: bitget,
-  },
-  // {
-  //   name: 'Bitmart',
-  //   icon: bitmart,
-  // },
+  
   {
     name: 'Pancakeswap',
     icon: pancakeswap,
@@ -35,19 +22,10 @@ const partners = [
     name: 'Mexc Global',
     icon: mexc,
   },
-  {
-    name: 'Uniswap',
-    icon: uniswap,
-  },
+ 
 
-  {
-    name: 'Bitrue',
-    icon: bitrue,
-  },
-  {
-    name: 'Raydium',
-    icon: raydium,
-  },
+ 
+ 
   {
     name: 'Gate.io',
     icon: Gate,
